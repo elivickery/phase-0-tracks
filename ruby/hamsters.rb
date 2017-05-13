2bbac6e6110f1980f58e39f171989eec41baf37e
Eli <eli@Elis-MacBook-Air.local>
2017-05-13 10:22:58 -0700

    Make a hamster program

---
+++ ruby/hamsters.rb
@@ -0,0 +1,30 @@
+def hamster_info
+    puts "What is the hamster's name? "
+    hamster_name = gets.chomp
+
+    puts "Hamster volume (1-10): "
+    hamster_volume = gets.chomp.to_i
+
+    puts "Fur color: "
+    hamster_color = gets.chomp
+
+    puts "Is the hamster a good candidate for adoption (Y/N)? "
+    adoption_fitness = gets.chomp
+    if adoption_fitness.upcase == 'Y'
+        adoption_fitness = true
+    else
+        adoption_fitness = false
+    end
+
+    puts "How old is the hamster?"
+    hamster_age = gets.chomp
+    if hamster_age == ''
+        hamster_age = nil
+    end
+
+end
+
+print hamster_info
+
+
+
