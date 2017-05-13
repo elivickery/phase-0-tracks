e05217dadc8ed46ce2b2e1c911f93059c33ca43c
Eli <eli@Elis-MacBook-Air.local>
2017-05-13 12:15:15 -0700

    Add questions

---
+++ ruby/vampires.rb
@@ -0,0 +1,17 @@
+def vampire_detector
+    puts "What is your name?"
+    name = gets.chomp
+
+    puts "How old are you?"
+    age = gets.chomp
+
+    puts "What year were you born?"
+    birth_year = gets.chomp
+
+    puts "Our company cafeteria serves garlic bread. Should we order some for you? (Y/N)"
+    garlic = gets.chomp
+
+    puts "Would you like to enroll in the company’s health insurance? (Y/N)"
+    insurance = gets.chomp
+
+end
