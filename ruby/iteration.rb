126e3f6a6482c17b033492994362b88db7209cad
Eli Vickery <e.n.vickery@gmail.com>
2017-05-17 13:40:31 -0700

    Add an array and a hash. Modify with .each and .map!

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -1,7 +1,36 @@
-def print_some_parameters
-	puts "Hello, world!"
-	yield("Hello", "World")
+# def print_some_parameters
+# 	puts "Hello, world!"
+# 	yield("Hello", "World")
+# end
+
+# print_some_parameters{|parameter1, parameter2| puts "What an awesome method! Here are our two parameters: #{parameter1} and #{parameter2}."}
+
+animals = ["cat", "dog", "elephant", "zebra"]
+
+favorite_colors = {:Joe => "blue", :Zelda => "red", :Caroline => "green"}
+
+p animals
+p favorite_colors
+
+
+# Using .each
+animals.each do |animal|
+	puts "A #{animal} is an animal."
 end
 
-print_some_parameters{|parameter1, parameter2| puts "What an awesome method! Here are our two parameters: #{parameter1} and #{parameter2}."}
+p animals
 
+# Using .each
+favorite_colors.each do |person, fav_color|
+	puts "#{person}'s favorite color is #{fav_color}."
+end
+
+p favorite_colors
+
+
+# Using .map!
+animals.map! do |animal|
+	animal = animal.upcase
+end
+
+p animals
