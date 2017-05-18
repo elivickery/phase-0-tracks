3eb8cc1f204cc90665eba28c8fd4e561b5cf4923
Eli Vickery <e.n.vickery@gmail.com>
2017-05-18 10:41:08 -0700

    Add interface method and loop

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,12 +1,12 @@
 def fake_name(given_name)
-	given_name = given_name.downcase.split(' ')
-	given_name[0], given_name[1] = given_name[1], given_name[0]
+	switched_name = given_name.downcase.split(' ')
+	switched_name[0], switched_name[1] = switched_name[1], switched_name[0]
 
 	vowels = ["a","e","i","o","u"]
 	alphabet = ("a".."z").to_a
 	consonants = alphabet - vowels
 
-	given_name.map! do |name_part|
+	switched_name.map! do |name_part|
 
 		name_part = name_part.chars()
 		
@@ -14,7 +14,7 @@ def fake_name(given_name)
 
 			if vowels.include? letter
 
-				if letter == u
+				if letter == 'u'
 					letter = vowels[0]
 				else
 					letter = vowels[vowels.index(letter) + 1]
@@ -22,7 +22,7 @@ def fake_name(given_name)
 
 			elsif consonants.include? letter
 
-				if letter == z
+				if letter == 'z'
 					letter = consonants[0]
 				else
 					letter = consonants[consonants.index(letter) + 1]
@@ -32,18 +32,24 @@ def fake_name(given_name)
 
 		end
 
+		name_part.join
+
 	end
 
-	given_name = given_name[0] + ' ' + given_name[1]
+	switched_name = switched_name[0].capitalize + ' ' + switched_name[1].capitalize
+
+	return given_name + ' is also known as ' + switched_name
 end
+
 
 def fake_name_interface
-
-	puts "Enter a name:"
-	given_name = gets.chomp
-	puts fake_name(given_name)
-
+	while true
+		puts 'Enter a name (or type "quit" to exit)'
+		given_name = gets.chomp
+		break if given_name == 'quit'
+		puts fake_name(given_name)
+	end
 end
 
 
-puts fake_name_interface
+fake_name_interface
