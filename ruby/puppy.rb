7135cfa9a1cab74dc31f6a3240fa8bc4ee9bc707
Eli Vickery <e.n.vickery@gmail.com>
2017-05-22 11:18:53 -0700

    Add puppy details

---
+++ ruby/puppy.rb
@@ -0,0 +1,19 @@
+SPECIES ------------------------
+Puppy
+
+CHARACTERISTICS ----------------
+Cuteness factor: 100%
+Friendly: yes
+Eye count: 2
+Name: varies
+Color: varies
+Legs: 4
+
+BEHAVIOR -----------------------
+Jump
+Lick
+Dig
+Sleep
+Eat
+
+
