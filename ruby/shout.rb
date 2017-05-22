bae31c1a99330fa0226085822b70b284064280a2
Eli Vickery <e.n.vickery@gmail.com>
2017-05-22 16:46:01 -0700

    Define Shout module and add yelling_happily method

---
+++ ruby/shout.rb
@@ -0,0 +1,11 @@
+module Shout
+  def self.yell_angrily(words)
+    words + "!!!" + " :("
+  end
+
+  def self.yelling_happily(words)
+    words + "!!!!" + " :)"
+  end
+end
+
+puts Shout.yelling_happily('Hello world')
