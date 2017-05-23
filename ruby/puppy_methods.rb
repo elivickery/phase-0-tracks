9591b3b46dd4c0005385fadd525d23ed4d4b21c6
Eli <eli@Elis-MacBook-Air.local>
2017-05-22 18:28:56 -0700

    Added a new party_planning method

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,34 +1,34 @@
-class Puppy
+# class Puppy
 
-  def initialize
-      puts "Initialize new puppy instance..."
-  end
+#   def initialize
+#       puts "Initialize new puppy instance..."
+#   end
 
-  def fetch(toy)
-    puts "I brought back the #{toy}!"
-    toy
-  end
+#   def fetch(toy)
+#     puts "I brought back the #{toy}!"
+#     toy
+#   end
 
-  def speak(x)
-    x.to_i.times { |x| puts "woof"}
-  end
+#   def speak(x)
+#     x.to_i.times { |x| puts "woof"}
+#   end
 
-  def rollover
-    puts '*rolls over*'
-  end
+#   def rollover
+#     puts '*rolls over*'
+#   end
 
-  def dog_years(age)
-    puts age.to_i * 7
-  end
+#   def dog_years(age)
+#     puts age.to_i * 7
+#   end
 
-  def play_dead
-    puts '**plays dead**'
-  end
+#   def play_dead
+#     puts '**plays dead**'
+#   end
 
-end
+# end
 
 
-puppy = Puppy.new
+# puppy = Puppy.new
 
 # puppy.fetch('ball')
 
@@ -39,3 +39,34 @@ puppy = Puppy.new
 # puppy.dog_years(7)
 
 # puppy.play_dead
+
+class Party_planning
+
+    def initialize
+        puts "Get the party started!"
+    end
+
+    def decorations
+        decoration_array = ['balloons','streamers','confetti','roses']
+
+        decoration_array.each do |decoration|
+            puts "We have #{decoration}!"
+        end
+    end
+
+    def release_the_doves(dove_number)
+        puts "Release #{dove_number} doves!!"
+    end
+end
+
+
+parties = []
+
+50.times do
+    party = Party_planning.new
+    parties << party
+end
+
+parties.each do |party|
+    party.release_the_doves(10)
+end
