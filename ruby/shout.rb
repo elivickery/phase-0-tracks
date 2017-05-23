6c32b0a905726c9ed7c1083d534f1073ea62645c
Eli Vickery <e.n.vickery@gmail.com>
2017-05-23 13:05:03 -0700

    Change shout method to a mixin

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,11 +1,35 @@
+# module Shout
+#   def self.yell_angrily(words)
+#     words + "!!!" + " :("
+#   end
+
+#   def self.yelling_happily(words)
+#     words + "!!!!" + " :)"
+#   end
+# end
+
+# puts Shout.yelling_happily('Hello world')
+
 module Shout
-  def self.yell_angrily(words)
+  def yell_angrily(words)
     words + "!!!" + " :("
   end
 
-  def self.yelling_happily(words)
+  def yelling_happily(words)
     words + "!!!!" + " :)"
   end
 end
 
-puts Shout.yelling_happily('Hello world')
+class Joe
+	include Shout
+end
+
+class Jane
+	include Shout
+end 
+
+joe = Joe.new
+puts joe.yell_angrily('Goodbye cruel world')
+
+jane = Jane.new
+puts jane.yelling_happily('Hello cruel world')
