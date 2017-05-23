d641a7751c110a85b397d0418e530ba4092ca183
Eli <eli@Elis-MacBook-Air.local>
2017-05-22 18:11:26 -0700

    Add Puppy class and methods

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -5,5 +5,33 @@ class Puppy
     toy
   end
 
+  def speak(x)
+    x.to_i.times { |x| puts "woof"}
+  end
+
+  def rollover
+    puts '*rolls over*'
+  end
+
+  def dog_years(age)
+    puts age.to_i * 7
+  end
+
+  def play_dead
+    puts '**plays dead**'
+  end
+
 end
 
+
+puppy = Puppy.new
+
+puppy.fetch('ball')
+
+puppy.speak(5)
+
+puppy.rollover
+
+puppy.dog_years(7)
+
+puppy.play_dead
