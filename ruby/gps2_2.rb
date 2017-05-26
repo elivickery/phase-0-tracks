06e3e954e88822c8327b102d98e85fbe8df02699
Eli <eli@Elis-MacBook-Air.local>
2017-05-25 20:11:14 -0700

    Added various methods

--- ruby/gps2_2.rb
+++ ruby/gps2_2.rb
@@ -2,12 +2,25 @@
 # input: string of items separated by spaces (example: "carrots apples cereal pizza")
 # steps:
   # Welcome and instructions
-  # Accept an input as a string
   # Ask for and set default quantity
   # Split string into keys in a hash (by spaces)
   # Set all values of the hash keys equal to the default quantity
   # Use print method to print the list to the console
 # output: hash
+
+def create_list(string)
+    puts "What is the default item quantity?"
+    default_quantity = gets.chomp.to_s
+    list_array = string.split(' ')
+    list_hash = {}
+    list_array.each do |item|
+        list_hash[item] = default_quantity
+    end
+
+    print_list(list_hash)
+
+    list_hash
+end
 
 # Method to add an item to a list
 # input: list, item name, and optional quantity
@@ -19,6 +32,28 @@
         # Otherwise default value = 1
 # output: hash
 
+def add_an_item(list_hash,item_name)
+    puts 'Adding the item: #{item_name}.'
+    puts 'Specify a quantity or type "default".'
+    quantity = gets.chomp.to_s
+
+    if quantity.downcase == 'default'
+        quantity = '1'
+    end
+
+    puts list_hash
+
+    if !list_hash[item_name]
+        puts 'Added the item: #{item_name}.'
+        list_hash[item_name] = quantity
+    else
+        puts 'Item already exists.'
+    end
+
+    list_hash
+
+end
+
 # Method to remove an item from the list
 # input: list, item name
 # steps:
@@ -27,6 +62,17 @@
     # If key exists, delete from the hash
 # output: hash
 
+def remove_an_item(list_hash,item_name)
+    if list_hash[item_name]
+        puts "Deleting item: #{item_name}."
+        list_hash.delete(item_name)
+    else
+        puts "Item does not exist."
+    end
+
+    list_hash
+end
+
 # Method to update the quantity of an item
 # input: list, item name, quantity
 # steps:
@@ -34,9 +80,27 @@
     # If key exists, update the value to the new quantity
 # output: hash
 
+def update_item_quantity(list_hash,item_name,quantity)
+    if list_hash[item_name]
+        puts "Updated the quantity of #{item_name} to: #{quantity}."
+        list_hash[item_name] = quantity
+    else
+        puts "Item does not exist."
+    end
+
+    list_hash
+end
+
 # Method to print a list and make it look pretty
 # input: list
 # steps:
     # Loop through the hash
     # Display each key-value pair as a statement
 # output: hash
+
+def print_list(list_hash)
+    list_hash.each do |key,value|
+        puts "The quantity of #{key} is: #{value}"
+    end
+end
+
