9c7b05d2a67e3d58aa39ff3e21529c77590fa480
Eli <eli@Elis-MacBook-Air.local>
2017-05-27 09:59:33 -0700

    Updated check_if_in_word method to check for multiple letter locations in word

--- ruby/game/game.rb
+++ ruby/game/game.rb
@@ -3,17 +3,24 @@
 # generate array from word
 # create an array of blanks
 # check if guessed letter is in the word
+# add letter to guessed array
+# check if letter has been guessed
 # add letter to blanks array
 # check whether word has been guessed
 # print the array in a readable way
 
 class Game
-
-    attr_accessor :word, :blanks_array
+    attr_reader :success_message, :fail_message
+    attr_accessor :word, :blanks_array, :guesses_available, :guessed_array, :letter_locations
 
     def initialize(word)
         @word = word
         @blanks_array = []
+        @guessed_array = []
+        @letter_locations = []
+        @guesses_available = @word.length
+        @success_message = "You guessed the word!"
+        @fail_message = "Sorry, you lose."
     end
 
 # Method: generate array from word
@@ -30,12 +37,30 @@ class Game
 
 # Method: check if guessed letter is in the word
     def check_if_in_word(letter)
-        @word.include? letter
+        @word.each do |item|
+            @letter_locations = []
+            if item == letter
+                @letter_locations = @word.each_index.select{|i| @word[i] == letter }
+                @letter_locations.each do |location|
+                    @blanks_array[location] = letter
+                end
+                true
+            else
+                false
+            end
+        end
     end
+
+
 
 # Method: add letter to blanks array
     def add_to_blanks_array(letter)
         @blanks_array[@word.index(letter)] = letter
+    end
+
+# Method: add letter to guessed array
+    def add_letter_to_guessed_array(letter)
+            @guessed_array << letter
     end
 
 # Method: check if word has been guessed
@@ -47,10 +72,70 @@ class Game
         end
     end
 
+# Method: check if letter has been guessed
+    def letter_has_been_guessed(letter)
+        if @guessed_array.include? letter
+            result = true
+        else
+            result = false
+        end
+    end
+
 # Method: print blanks array in a readable way
     def print_blanks_array
         @blanks_array.join(' ')
     end
 
 end
+
+## User Interface
+
+puts "Welcome, USER1! Enter a word:"
+
+game = Game.new(gets.chomp)
+
+game.generate_word_array
+
+game.generate_blanks_array
+
+puts "USER2: Let's guess the word."
+
+loop do
+    if game.guesses_available > 0
+
+        puts game.print_blanks_array
+
+        puts "Guess a letter:"
+
+        letter = gets.chomp
+
+        if !game.letter_has_been_guessed(letter)
+            game.add_letter_to_guessed_array(letter)
+            game.guesses_available -= 1
+        else
+            puts "You guessed that letter already."
+        end
+
+        game.check_if_in_word(letter)
+
+        if game.word_has_been_guessed
+            puts game.success_message
+            break
+        else
+            puts "You have #{game.guesses_available} guesses left."
+        end
+    else
+        puts game.fail_message
+        break
+    end
+
+end
+
+
+
+
+
+
+
+
 
--- ruby/game/game_spec.rb
+++ ruby/game/game_spec.rb
@@ -2,6 +2,8 @@
 # generate array from word
 # create an array of blanks
 # check if guessed letter is in the word
+# add letter to guessed array
+# check if letter has been guessed
 # add letter to blanks array
 # check whether word has been guessed
 # print the array in a readable way
@@ -11,35 +13,45 @@ require_relative 'game'
 describe Game do
   let(:game) { Game.new('potato') }
 
-it "generates an array from the word" do
-  game.generate_word_array
-  expect(game.word).to eq ['p','o','t','a','t','o']
-end
+  it "generates an array from the word" do
+    game.generate_word_array
+    expect(game.word).to eq ['p','o','t','a','t','o']
+  end
 
-it "generates an array of blanks" do
-  game.generate_blanks_array
-  expect(game.blanks_array).to eq ['_','_','_','_','_','_']
-end
+  it "generates an array of blanks" do
+    game.generate_blanks_array
+    expect(game.blanks_array).to eq ['_','_','_','_','_','_']
+  end
 
-it "checks if letter is in the word" do
-  expect(game.check_if_in_word('p')).to eq true
-end
+  it "checks if letter is in the word" do
+    expect(game.check_if_in_word('p')).to eq true
+  end
 
-it "adds the letter to the blanks array" do
-  game.add_to_blanks_array('p')
-  expect(game.blanks_array).to eq ['p']
-end
+  it "adds the letter to the guessed array" do
+    game.add_letter_to_guessed_array('p')
+    expect(game.guessed_array).to eq ['p']
+  end
 
-it "checks if the word has been guessed" do
-  game.generate_word_array
-  game.blanks_array = ['p','o','t','a','t','o']
-  expect(game.word_has_been_guessed).to eq true
-end
+  it "adds the letter to the blanks array" do
+    game.add_to_blanks_array('p')
+    expect(game.blanks_array).to eq ['p']
+  end
 
-it "prints the blanks array in a readable way" do
-  game.generate_word_array
-  game.generate_blanks_array
-  expect(game.print_blanks_array).to eq '_ _ _ _ _ _'
-end
+  it "checks if letter has been guessed" do
+    game.add_letter_to_guessed_array('p')
+    expect(game.letter_has_been_guessed('p')).to eq true
+  end
+
+  it "checks if the word has been guessed" do
+    game.generate_word_array
+    game.blanks_array = ['p','o','t','a','t','o']
+    expect(game.word_has_been_guessed).to eq true
+  end
+
+  it "prints the blanks array in a readable way" do
+    game.generate_word_array
+    game.generate_blanks_array
+    expect(game.print_blanks_array).to eq '_ _ _ _ _ _'
+  end
 
 end
