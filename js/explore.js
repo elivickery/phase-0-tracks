0f16ba630689e143f4d38d7cbd0618fdb1257d76
Eli <eli@Elis-MacBook-Air.local>
2017-05-30 10:54:41 -0700

    Add pseudocode

---
+++ js/explore.js
@@ -0,0 +1,8 @@
+// REVERSE A STRING
+
+// define a function 'reverseString' that takes one parameter (string)
+// create a for loop until i = the length of the string - 1:
+    // for each letter in the string,
+        // add it to the end of the string, and
+        // set the 0 index of the string equal to nil
+    // return the string
