d31d0975caab1fa5d7a69056496ef644cadd67ea
Eli <eli@Elis-MacBook-Air.local>
2017-05-30 12:32:33 -0700

    Added object

--- js/data_structures.js
+++ js/data_structures.js
@@ -11,3 +11,14 @@ names.push('Gertrude');
 console.log(colors);
 
 console.log(names);
+
+
+// Set up the object
+
+horses = {};
+
+for(var i = 0; i < names.length; i++) {
+    horses[names[i]] = colors[i];
+}
+
+console.log(horses);
