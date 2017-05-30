864226f77d2da276da41b61af55923ca8c469e68
Eli <eli@Elis-MacBook-Air.local>
2017-05-30 12:25:16 -0700

    Add initial arrays

---
+++ js/data_structures.js
@@ -0,0 +1,13 @@
+// Set up arrays
+
+var colors = ['red','blue','glitter','purple'];
+
+var names = ['Ed','Reina','James','Beauty'];
+
+colors.push('pink');
+
+names.push('Gertrude');
+
+console.log(colors);
+
+console.log(names);
