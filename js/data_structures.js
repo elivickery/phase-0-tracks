5e751d1ea3c27479240db28f4cbbf43b2d1d166f
Eli <eli@Elis-MacBook-Air.local>
2017-05-30 13:04:20 -0700

    Add the car object

--- js/data_structures.js
+++ js/data_structures.js
@@ -22,3 +22,22 @@ for(var i = 0; i < names.length; i++) {
 }
 
 console.log(horses);
+
+
+// Create a car object
+
+function Car(color, make, speed) {
+    this.color = color;
+    this.make = make;
+    this.speed = speed;
+    this.drive = function(speed, make) { console.log("The " + make + " is driving " + speed + " mph."); };
+}
+
+var car = new Car("Red", "Mazda", 100);
+var car1 = new Car("Green", "Ford", 35);
+var car2 = new Car("Yellow", "Mustang", 50);
+
+console.log(car.color);
+car.drive(car.speed, car.make);
+car1.drive(car1.speed, car1.make);
+car2.drive(car2.speed, car2.make);
