c49cf8b630532b761a8f1a22a1abba75c0621611
Eli <eli@Elis-MacBook-Air.local>
2017-06-02 10:58:48 -0700

    Add function and driver code

--- js/algos.js
+++ js/algos.js
@@ -5,3 +5,22 @@
     // For through each item in the array
         // Loop through all items and compare item length to length of longestString
         // If item is longer than longestString, reassign longestString = item
+
+var test_array = ["long phrase","longest phrase","longer phrase"];
+
+function findLongest(array) {
+
+    var longestString = "";
+
+    for(var i = 0; i < array.length; i++) {
+
+        if(array[i].length > longestString.length) {
+            longestString = array[i];
+        }
+    }
+
+    return(longestString);
+
+}
+
+console.log(findLongest(test_array));
