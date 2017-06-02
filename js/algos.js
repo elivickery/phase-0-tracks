43fba75cbfcaa9774715230ea4ac06a9c0b8d674
Eli <eli@Elis-MacBook-Air.local>
2017-06-02 10:58:21 -0700

    Initial commit w/ pseudocode

---
+++ js/algos.js
@@ -0,0 +1,7 @@
+// Method: Find longest string
+// Input: Array of strings
+// Steps:
+    // Create empty variable 'longestString' representing the longest string
+    // For through each item in the array
+        // Loop through all items and compare item length to length of longestString
+        // If item is longer than longestString, reassign longestString = item
